bc61d21d115c4914583c4ed3173139a6e2289bc4
StevenWarmelink <stevenwarmelink@gmail.com>
2023-09-19 14:00:56 +0200

    updated

---
+++ Assets/GameManager.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if(_instance is null)
+                Debug.Log("Game Manager is NULL");
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public GameObject pickup_good;
+    public GameObject pickup_bad;
+   
+    [Range(1, 50)]
+    public int arena_extents = 1;
+
+    [Range(1, 50)]
+    public int n_pickups = 15;
+    
+    private List<GameObject> good_pellets = new List<GameObject>();
+    private List<GameObject> bad_pellets = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        create_arena();
+        spawn_pellets();
+    }
+
+    void create_arena()
+    {
+        GameObject floor_obj = transform.parent.Find("Floor").gameObject;
+        floor_obj.transform.localScale = Vector3.one * arena_extents;
+
+
+        Transform[] allWalls = transform.parent.GetComponentsInChildren<Transform>(true);
+        for (int i = 0; i < allWalls.Length; i++)
+        { 
+            if (allWalls[i].name == "Wall")
+            {
+                Vector3 localpos = allWalls[i].transform.localPosition;
+
+                allWalls[i].transform.localPosition = new Vector3(arena_extents * localpos.x, 
+                                                                .08f, 
+                                                                arena_extents * localpos.z);
+                allWalls[i].transform.localScale = new Vector3(.15f, 
+                                                                1f, 
+                                                                arena_extents * 10f);
+            }
+        }
+    }
+
+    void spawn_pellet(GameObject pickup, List<GameObject> pickup_list)
+    {
+        float x, y, z;
+
+        x = Random.Range(-4.5f * arena_extents, 4.5f * arena_extents);
+        y = 0.5f;
+        z = Random.Range(-4.5f * arena_extents, 4.5f * arena_extents);
+
+        Vector3 new_pos = new Vector3(x, y, z);
+        new_pos += transform.parent.transform.position;
+
+        GameObject pellet = (GameObject)Instantiate(pickup, new_pos, Quaternion.identity);
+
+        pickup_list.Add(pellet);
+    }
+
+    public void spawn_pellets()
+    {
+        destroy_old_pellets();
+
+        for (int idx = 0; idx < n_pickups; idx++)
+        {
+            spawn_pellet(pickup_good, good_pellets);
+            spawn_pellet(pickup_bad, bad_pellets);
+        }
+    }
+
+    void destroy_old_pellets()
+    {
+        foreach(GameObject obj in good_pellets)
+            Destroy(obj);
+        foreach(GameObject obj in bad_pellets)
+            Destroy(obj);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/MyAgent.cs
+++ Assets/MyAgent.cs
@@ -12,84 +12,21 @@ public class MyAgent : Agent
     public float forceMultiplier = 10f;
     public int n_steps_per_run = 3000;
 
-    [Range(1, 50)]
-    public int arena_extents = 1;
+    // [Range(1, 50)]
+    // public int arena_extents = 1;
     private int epoch;
 
-
-    
-    // Start is called before the first frame update
     void Start()
     {
         my_rbody = GetComponent<Rigidbody>();  
-        // public float arena_extents = 1f;
-        GameObject floor_obj = transform.parent.Find("Floor").gameObject;
-        floor_obj.transform.localScale = Vector3.one * arena_extents;
-
-
-        Transform[] allWalls = transform.parent.GetComponentsInChildren<Transform>(true);
-        for (int i = 0; i < allWalls.Length; i++)
-        { 
-            // GameObject wall_obj = transform.parent.Find("Wall").gameObject;
-            // if (wall_obj)
-            // {
-            //     // print(wall_obj);
-            //     Vector3 localpos = wall_obj.transform.localPosition;
-
-            //     print(localpos);
-            //     wall_obj.transform.localPosition = new Vector3(arena_extents * localpos.x, 
-            //                                                     .08f, 
-            //                                                     arena_extents * localpos.z);
-            // }
-            if (allWalls[i].name == "Wall")
-            {
-                Vector3 localpos = allWalls[i].transform.localPosition;
-
-                allWalls[i].transform.localPosition = new Vector3(arena_extents * localpos.x, 
-                                                                .08f, 
-                                                                arena_extents * localpos.z);
-                allWalls[i].transform.localScale = new Vector3(.15f, 
-                                                                1f, 
-                                                                arena_extents * 10f);
-                // print(allWalls[i].)
-            }
-            // print(allWalls.name);
-        }
+       
     }
-    
+
+
 
     
-
-
     public override void OnEpisodeBegin()
     {
-        // var localScale = transform.parent.Find("Floor");
-
-        
-
-
-
-        //Array to hold all child obj
-        // GameObject[] allChildren = new GameObject[transform.childCount];
-        // }
-        // {
-
-        // }
-        // // int i = 0;
-        // // int n_walls = 4;
-        // // for  
-        // // foreach (var go in transform.parent.Find("Wall").gameObject)
-        // {
-        //     // GameObject[i] = go;
-        //     print(go);
-        //     float x_pos = 5 * arena_extents;
-        //     float z_pos = 5 * arena_extents;
-        //     //  = arena_extents;
-        //     go.transform.localPosition = new Vector3(x_pos, .08f, z_pos);
-        //     // print(go.transform.localPosition);
-        // }
-
-        print("OnEpisodeBegin()");
         epoch = 0;
         if (this.transform.localPosition.y < 0)
         {
@@ -98,9 +35,9 @@ public class MyAgent : Agent
             this.transform.localPosition  = new Vector3(0, 0, 0);
         }
 
-        Target.localPosition = new Vector3(Random.value * 8 - 4, 
-                                           0, 
-                                           Random.value * 8 - 4) * arena_extents;
+        Target.localPosition = new Vector3((Random.value * 8 - 4) * GameManager.Instance.arena_extents, 
+                                           0.54f, 
+                                           (Random.value * 8 - 4) * GameManager.Instance.arena_extents);
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -118,57 +55,38 @@ public class MyAgent : Agent
             AddReward(-.5f);
             EndEpisode();
         }
-        // Vector3 controlSignal = Vector3.zero;
-        // controlSignal.x = actionBuffers.ContinuousActions[0] / 5;
-        // controlSignal.z = actionBuffers.ContinuousActions[1] / 100;
-
-        // Vector3 currentPos = this.transform.position;
-        // currentPos += controlSignal;
-        // this.transform.position += controlSignal; 
         this.transform.Rotate(0f, actionBuffers.ContinuousActions[1] * 2, 0f);
-        // my_rbody.AddForce(controlSignal * forceMultiplier);
 
         my_rbody.velocity = transform.forward * Mathf.Max(0f, actionBuffers.ContinuousActions[0]) * forceMultiplier;
 
-        // float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
-
-        // if (distanceToTarget <= 1.42f)
-        // {
-        //     AddReward(10.0f);
-        //     EndEpisode();
-        // }
-        // else if (this.transform.localPosition.y < 0)
-        // {
-        //     SetReward(0f);
-        //     EndEpisode();
-        // }
-        // else
         float penalty_for_not_moving = 1 - Mathf.Max(0f, actionBuffers.ContinuousActions[0]);
-        print("penalty: " + penalty_for_not_moving);
+        // print("penalty: " + penalty_for_not_moving);
         AddReward(-.05f * penalty_for_not_moving);
-        // AddReward(-0.001f);
-        // }
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Wall")
+        switch(collision.gameObject.tag)
         {
-            AddReward(-1f);
-            // Vector3 spawnpos = Vector3(0f, .08f, 0f) 
-            transform.localPosition = Vector3.zero;
-            EndEpisode();
-        }
-        else if (collision.gameObject.tag == "Target")
-        {
-            AddReward(100f);
-            EndEpisode();
-        }
+            case "GoodPellet":
+                AddReward(20f);
+                Destroy(collision.gameObject);
+                break;
+            case "BadPellet":
+                AddReward(-20f);
+                Destroy(collision.gameObject);
+                break;
+            case "Wall":
+                AddReward(-50f);
+                transform.localPosition = Vector3.zero;
+                GameManager.Instance.spawn_pellets();
+                EndEpisode();
+                break;
+            case "Target":
+                AddReward(100f);
+                GameManager.Instance.spawn_pellets();
+                EndEpisode();
+                break;
+        }        
     }
-
-    // Update is called once per frame
-    // void Update()
-    // {
-        
-    // }
 }
